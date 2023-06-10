da6826e8877bb11e47f39e2a1b113c2e8eaacd73
Elxem000 <135558430+Elxem000@users.noreply.github.com>
2023-06-11 00:51:21 +0300

    Update ElxUpdate.sh

--- ElxUpdate.sh
+++ ElxUpdate.sh
@@ -1,1 +1,21 @@
+cd ..
 
+rm -fr ElxTc0lu-turucu
+
+clear
+
+rmdir ElxTc0lu-turucu
+
+clear
+
+git clone https://github.com/Elxem000/ElxTc0lu-turucu
+
+clear
+
+figlet "JuanE" | lolcat
+
+cd
+
+echo "ElxTc0lu-turucu güncelendi."
+
+echo "(⁠;ꈍ⁠ᴗ⁠ꈍ⁠)"
