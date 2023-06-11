2639826e1dbff2b1dde8defb4d93ecd51ebd8389
Elxem000 <135558430+Elxem000@users.noreply.github.com>
2023-06-11 06:21:19 +0300

    Update ElxTc.py

--- ElxTc.py
+++ ElxTc.py
@@ -5,7 +5,7 @@ os.system("clear")
 print("ElxTc açılıyor …⁠ᘛ⁠⁐̤⁠ᕐ⁠ᐷ")
 time.sleep(3)
 os.system("clear")
-while abc != int(1 and 2):
+while abc != int(1 and :
     print("|---------------------|(⁠⊙⁠_⁠◎⁠)")
     print("| 1 => TC oluşturmak. |")
     print("| 2 => TC sorgulamak. |")
