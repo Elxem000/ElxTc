d4d1aacd014549299705e8d064f524730dc1c4c9
Elxem000 <135558430+Elxem000@users.noreply.github.com>
2023-06-11 04:28:26 +0300

    Update ElxTc.py

--- ElxTc.py
+++ ElxTc.py
@@ -7,7 +7,12 @@ time.sleep(3)
 os.system("clear")
 while True:
     abc=int(input("Hangisini İstersiniz"))
-    if abc==1:
+    if abc!=int(1 and 2):
+        print("Lütfen sadece 1 veya 2 giriniz.")
+        print("(;⁠ー⁠_⁠ー⁠)")
+        continue
+ 
+    else abc==1:
 
         while True:
             os.system("clear")
@@ -27,28 +32,28 @@ while True:
                 print("ฅ⁠^⁠•⁠ﻌ⁠•⁠^⁠ฅ")
                 continue
             break 
-        break
-    print("TAMAMDIR")
-    print("(⁠^⁠.⁠_⁠.⁠^⁠)⁠ﾉ")
-    time.sleep(3)
-    i=1
+        print("TAMAMDIR")
+        print("(⁠^⁠.⁠_⁠.⁠^⁠)⁠ﾉ")
+        time.sleep(3)
+        i=1
 
-    while i<=adet:
-        a1=random.randint(1,9)
-        a2=random.randint(0,9)
-        a3=random.randint(0,9)
-        a4=random.randint(0,9)
-        a5=random.randint(0,9)
-        a6=random.randint(0,9)
-        a7=random.randint(0,9)
-        a8=random.randint(0,9)
-        a9=random.randint(0,9)
-        a10=((((a1+a3+a5+a7+a9)*7)-(a2+a4+a6+a8))%10)
-        a11=((a1+a2+a3+a4+a5+a6+a7+a8+a9+a10)%10)
-       print("________________________________________")
-       print(i,".TC KİMLİK NO:",a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11)
-       print("________________________________________")
-       i=i+1
-       time.sleep(0.5)
-    print("NOT:!!!Numaraların aralarında boşluklar vardır!!!")
-    print("<⁠(⁠￣⁠︶⁠￣⁠)⁠↗")
+        while i<=adet:
+            a1=random.randint(1,9)
+            a2=random.randint(0,9)
+            a3=random.randint(0,9)
+            a4=random.randint(0,9)
+            a5=random.randint(0,9)
+            a6=random.randint(0,9)
+            a7=random.randint(0,9)
+            a8=random.randint(0,9)
+            a9=random.randint(0,9)
+            a10=((((a1+a3+a5+a7+a9)*7)-(a2+a4+a6+a8))%10)
+            a11=((a1+a2+a3+a4+a5+a6+a7+a8+a9+a10)%10)
+            print("________________________________________")
+            print(i,".TC KİMLİK NO:",a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11)
+            print("________________________________________")
+            i=i+1
+            time.sleep(0.5)
+            print("NOT:!!!Numaraların aralarında boşluklar vardır!!!")
+            print("<⁠(⁠￣⁠︶⁠￣⁠)⁠↗")
+    break 
