6b4ba1814e1cc4275a5a54536f8adbbb4d0ec651
Elxem000 <135558430+Elxem000@users.noreply.github.com>
2023-06-11 13:22:24 +0300

    Create SetupTermx.sh

---
+++ SetupTermx.sh
@@ -0,0 +1,18 @@
+
+clear
+echo "ElxTc kuruluyor…⁠ᘛ⁠⁐̤⁠ᕐ⁠ᐷ"
+
+pkg upgrade 
+pkg upgrade
+
+pkg install python
+pkg install python3
+pkg install python2 
+
+pip install random
+pip install os
+pip install time
+
+clear
+
+echo "(⁠＾⁠3⁠＾⁠♪ ElxTc Kurulumu biti."
