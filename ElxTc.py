e36119eb3e925c199127c9e5f60a8d734ddaa8d7
Elxem000 <135558430+Elxem000@users.noreply.github.com>
2023-06-11 07:56:41 +0300

    Update ElxTc.py

--- ElxTc.py
+++ ElxTc.py
@@ -63,29 +63,28 @@ while True:
    
             
     elif(abc == 2 ):
-        while True: 
-           def encodeTc(tc):
-               sum = 0
-               for i in tc:
-                   sum += int(i)
-               tc+= str(sum)[-1]
-               return tc
-           def decodeTc(girilen_tc):
-               if len(girilen_tc)!=11:
-                   print("Geçerli bir TC Kimlik Numarası giriniz!")
-                   return
-               encodedtc=encodeTc(girilen_tc[0:10])
-               return girilen_tc == encodedtc
-           def tclogin():
-               a = input("Tc giriniz:")
-               result = decodeTc(a)
-               if result == None:
-                   quit()
-               elif not result:
-                   print("Girdiğiniz TC Kimlik numarası gerçek değildir.")
-               elif result:
-                   print("Geçerli Bir TC Kimlik numarası girdiniz.")
-            tclogin()
+       def encodeTc(tc):
+           sum = 0
+           for i in tc:
+               sum += int(i)
+            tc+= str(sum)[-1]
+            return tc
+       def decodeTc(girilen_tc):
+           if len(girilen_tc)!=11:
+               print("Geçerli bir TC Kimlik Numarası giriniz!")
+               return
+           encodedtc=encodeTc(girilen_tc[0:10])
+           return girilen_tc == encodedtc
+       def tclogin():
+           a = input("Tc giriniz:")
+           result = decodeTc(a)
+           if result == None:
+               quit()
+           elif not result:
+               print("Girdiğiniz TC Kimlik numarası gerçek değildir.")
+           elif result:
+               print("Geçerli Bir TC Kimlik numarası girdiniz.")
+       tclogin()
                 
 
         
