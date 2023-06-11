7191865e8fdd74f27a091b6a7785ba40144a2243
Elxem000 <135558430+Elxem000@users.noreply.github.com>
2023-06-11 05:02:57 +0300

    Update ElxTc.py

--- ElxTc.py
+++ ElxTc.py
@@ -43,12 +43,12 @@ while True:
                 a7=random.randint(0,9)
                 a8=random.randint(0,9)
                 a9=random.randint(0,9)
-                 a10=((((a1+a3+a5+a7+a9)*7)-(a2+a4+a6+a8))%10)
-                 a11=((a1+a2+a3+a4+a5+a6+a7+a8+a9+a10)%10)
-                 print("________________________________________")
-                 print(i,".TC KİMLİK NO:",a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11)
-                 print("________________________________________")
-                 i=i+1
+                a10=((((a1+a3+a5+a7+a9)*7)-(a2+a4+a6+a8))%10)
+                a11=((a1+a2+a3+a4+a5+a6+a7+a8+a9+a10)%10)
+                print("________________________________________")
+                print(i,".TC KİMLİK NO:",a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11)
+                print("________________________________________")
+                i=i+1
                 time.sleep(0.5)
                 print("NOT:!!!Numaraların aralarında boşluklar vardır!!!")
                 print("<⁠(⁠￣⁠︶⁠￣⁠)⁠↗")
